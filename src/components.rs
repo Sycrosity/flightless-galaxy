d865d37550fb9f550abb1de82ed5fe1ed20640f8
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-01-23 22:57:55 +0000

    added the parent planet struct for entity children

--- src/components.rs
+++ src/components.rs
@@ -9,10 +9,6 @@ use crate::prelude::*;
 #[reflect(Component)]
 pub struct Player;
 
-#[derive(Component, Reflect, Default)]
-#[reflect(Component)]
-pub struct Planet;
-
 ///a [`Component`] to display if the entity can be controlled by actions.
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)]
@@ -29,6 +25,45 @@ impl Controllable {
 #[derive(Component, Reflect, Default, InspectorOptions)]
 #[reflect(Component, InspectorOptions)]
 pub struct Speed(#[inspector(min = 0.0, max = 256.0, display = NumberDisplay::Slider)] pub f32);
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct Planet;
+
+#[derive(Bundle)]
+pub struct PlanetBundle {
+    _player: Player,
+    pos: Polar,
+    #[bundle]
+    sprite_bundle: SpriteBundle,
+    // mesh: Mesh,
+    #[bundle]
+    input_manager: InputManagerBundle<GameAction>,
+    // sprite_sheet_bundle: SpriteSheetBundle,
+}
+
+impl PlanetBundle {
+    pub fn new(pos: Polar, texture: Handle<Image>) -> Self {
+        Self {
+            _player: Player,
+            pos,
+            // mesh: shape::Circle::new(100.).into(),
+            sprite_bundle: SpriteBundle {
+                texture,
+                transform: Transform::from_polar_seperate(
+                    Polar::new(pos.r, pos.theta, pos.z),
+                    Quat::from_rotation_z(pos.rotation()),
+                    Vec3::splat(0.1),
+                ),
+                ..default()
+            },
+            input_manager: InputManagerBundle {
+                input_map: PlayerBundle::default_input_map(),
+                ..default()
+            },
+        }
+    }
+}
 
 #[derive(Bundle)]
 pub struct PlayerBundle {
@@ -62,75 +97,87 @@ impl PlayerBundle {
         }
     }
 
+    //for when some types of input aren't used
+    #[allow(unused)]
+    //the default, no-file found input map setup
     pub fn default_input_map() -> InputMap<GameAction> {
         // This allows us to replace `ArpgAction::Up` with `Up`,
         // significantly reducing boilerplate
         use GameAction::*;
+        // use InputKind::*;
+        type Key = KeyCode;
+        // type Scan = ScanCode;
+        type Mouse = MouseButton;
+        type Gamepad = GamepadButtonType;
+
         let mut input_map = InputMap::default();
 
         //movement
-        input_map.insert(KeyCode::Up, Up);
-        input_map.insert(KeyCode::W, Up);
-        input_map.insert(GamepadButtonType::DPadUp, Up);
+        // .insertScanCode, Up)
+        input_map
+            .insert(Key::W, Up)
+            .insert(Key::Up, Up)
+            .insert(Gamepad::DPadUp, Up)
 
-        input_map.insert(KeyCode::Down, Down);
-        input_map.insert(KeyCode::S, Down);
-        input_map.insert(GamepadButtonType::DPadDown, Down);
+            .insert(Key::S, Down)
+            .insert(Key::Down, Down)
+            .insert(Gamepad::DPadDown, Down)
 
-        input_map.insert(KeyCode::Left, Left);
-        input_map.insert(KeyCode::A, Left);
-        input_map.insert(GamepadButtonType::DPadLeft, Left);
+            .insert(Key::A, Left)
+            .insert(Key::Left, Left)
+            .insert(Gamepad::DPadLeft, Left)
 
-        input_map.insert(KeyCode::Right, Right);
-        input_map.insert(KeyCode::D, Right);
-        input_map.insert(GamepadButtonType::DPadRight, Right);
+            .insert(Key::D, Right)
+            .insert(Key::Right, Right)
+            .insert(Gamepad::DPadRight, Right)
 
-        input_map.insert(KeyCode::Space, Jump);
-        input_map.insert(GamepadButtonType::South, Jump);
+            .insert(Key::Space, Jump)
+            .insert(Gamepad::North, Jump)
+            //abilities
+            // .insert(Key::Q, Ability1)
+            // .insert(Gamepad::West, Ability1)
+            .insert(Mouse::Left, Primary)
+            // .insert(Key::W, Ability2)
+            .insert(Gamepad::North, Primary)
+            .insert(Mouse::Right, Secondary)
+            // .insert(Key::E, Ability3)
+            // .insert(Gamepad::East, Ability3)
+            // .insert(Key::R, Ultimate)
+            // .insert(Gamepad::LeftTrigger2, Ultimate)
+            ;
 
-        //abilities
-        // input_map.insert(KeyCode::Q, Ability1);
-        // input_map.insert(GamepadButtonType::West, Ability1);
-        input_map.insert(MouseButton::Left, Primary);
+        let raw = r#"
+            InputMap(
+                map: {
+                    Run:  [Single(Keyboard(LShift)),Single(Keyboard(RShift))],
+                    Jump: [Single(Keyboard(Space))],
+                    Hide: [Chord([Some(Keyboard(R)),Some(Keyboard(E)),None,None,None,None,None,None])]
+                }
+            )
+        "#;
 
-        // input_map.insert(KeyCode::W, Ability2);
-        // input_map.insert(GamepadButtonType::North, Ability2);
-        input_map.insert(MouseButton::Right, Secondary);
+        let b: ron::error::SpannedResult<InputMap<GameAction>> =
+            ron::de::from_bytes(include_bytes!("../assets/test.ron"));
 
-        // input_map.insert(KeyCode::E, Ability3);
-        // input_map.insert(GamepadButtonType::East, Ability3);
+        info!("{:?}", &b);
 
-        // input_map.insert(KeyCode::R, Ultimate);
-        // input_map.insert(GamepadButtonType::LeftTrigger2, Ultimate);
+        let raw = r#"
+            InputMap(
+                map: {
+                    Run:  [Single(Keyboard(LShift)),Single(Keyboard(RShift))],
+                    Jump: [Single(Keyboard(Space))],
+                    Hide: [Chord([Some(Keyboard(R)),Some(Keyboard(E)),None,None,None,None,None,None])]
+                }
+            )
+        "#;
+
+        let b: ron::error::SpannedResult<InputMap<GameAction>> = ron::de::from_str(&raw);
+
+        info!("{:?}", &b);
 
         input_map
     }
 }
-
-///a set of coordinates in the [polar coordinate system](https://en.wikipedia.org/wiki/Polar_coordinate_system).
-// #[derive(Clone, Copy, Default, Component)]
-// pub struct PolarPos {
-//     /// The radius (distance) from the reference pole.
-//     r: f32,
-//     /// The polar angle from the reference direction.
-//     theta: f32,
-// }
-
-// impl PolarPos {
-//     /// Creates a new [`PolarPos`].
-//     pub fn new(r: f32, theta: f32) -> Self {
-//         Self { r, theta }
-//     }
-
-//     // /// Creates a [`PolarPos`] at 0,0.
-//     pub fn default() -> Self {
-//         Self { r: 0., theta: 0. }
-//     }
-
-//     pub fn rotation(self) -> f32 {
-//         self.theta.to_radians()
-//     }
-// }
 
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)]
